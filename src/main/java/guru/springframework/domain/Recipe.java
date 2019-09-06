fa9d74b9397ea9598fab5f6a5a437f9619416d3b
presscorner <denis.prescornic@gmail.com>
2019-09-06 15:51:02 -0400

    Added Category entity and ManyToMany relationship

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -30,6 +30,10 @@ public class Recipe {
     @OneToOne(cascade = CascadeType.ALL) // if we delete a Recipe, we will delete the notes also
     private Notes notes;
 
+    @ManyToMany
+    @JoinTable(name = "recipe_category", joinColumns = @JoinColumn(name = "recipe_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
+
     public Long getId() {
         return id;
     }
@@ -124,5 +128,13 @@ public class Recipe {
 
     public void setDifficulty(Difficulty difficulty) {
         this.difficulty = difficulty;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
