85bebe317dcc1ba052fbf3be1d783363a706b1cf
Denis Prescornic <denis.prescornic@gmail.com>
2019-09-12 23:27:40 -0400

    1) Adding bootstrap data 2) Changed controller to get recipes list 3) Added missing fields and initialisations to entities 4) Added Optional to repositories 5) Created services 6) Changed template for recipes list 7) Added missing data to sql file

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -12,11 +12,18 @@ public class Ingredient {
     private String description;
     private BigDecimal amount;
 
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure unitOfMeasure;
+
     @ManyToOne
     private Recipe recipe;
 
-    @OneToOne(fetch = FetchType.EAGER)
-    private UnitOfMeasure unitOfMeasure;
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure unitOfMeasure, Recipe recipe) {
+        this.description = description;
+        this.amount = amount;
+        this.unitOfMeasure = unitOfMeasure;
+        this.recipe = recipe;
+    }
 
     public Long getId() {
         return id;
